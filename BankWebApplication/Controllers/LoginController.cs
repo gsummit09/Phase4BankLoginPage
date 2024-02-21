4700c4346de2e924c18d427d8ab3aabe93ba1f93
gsummit09 <nayaksumit998@gmail.com>
2024-02-21 23:49:37 +0530

    Add project files.

---
+++ BankWebApplication/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            return RedirectToAction("Dashboard", "Home", new { username = username });
+        }
+    }
+}
